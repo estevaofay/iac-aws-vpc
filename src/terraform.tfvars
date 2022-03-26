81a144b7920a4ae4347cd0cb977cb01f9f982924
Estêvão Francisco Fáy <estevao@getmentum.com>
2022-03-26 18:10:42 -0300

    feat(vpc-module): creates initial vpc infra-as-code
    creates initial terraform files for vpc creation
    
    BREAKING CHANGE:

---
+++ src/terraform.tfvars
@@ -0,0 +1,3 @@
+vpc_cidr = "172.31.0.0/16"
+
+region = "us-east-1"
---
+++ src/variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_cidr" {
+  type        = string
+  description = "The CIDR block of the VPC"
+}
+
+variable "region" {
+  type        = string
+  description = "The region where the VPC will be created"
+}
